1a0f77aa6fa49cdcdf9513624938657328147bba
Daniel Rodrigues <danielrodriguesdrs331@gmail.com>
2019-07-27 14:01:17 -0300

    new(): Create factories

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
   has_many :teams
   has_many :messages
-  has_many :talks
-  
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :talks, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 end
